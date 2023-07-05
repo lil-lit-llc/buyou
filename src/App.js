42a292f2b898e3de1ce33110656e8837ac3023db
Gen Wang <gen.wang@airbnb.com>
2023-07-04 18:23:20 -0700

    remove build errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from './Home';
+import React, {useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/home.html");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <></>
   );
 }
 
--- src/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Iframe from './Iframe';
-
-export default function Home() {
-    return (
-        <Iframe src="/home.html" />
-    );
-}
